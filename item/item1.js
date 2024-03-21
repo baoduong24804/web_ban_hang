867da9ec6d585328a68e3f2d9172ece341760c65
baoduong <baoduong24804@gmail.com>
2024-03-21 21:41:58 +0700

    commit2

---
+++ item/item1.js
@@ -0,0 +1,38 @@
+[
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/4d0a4ba0f5aa7cf007e1d2041e49323f.png_400x400q80.png",
+        "thongtin":"[PIN TRÂU 30000MAH - TẶNG CÁP SẠC NHANH] PIN SẠC DỰ PHÒNG CAO CẤP THIẾT KẾ MÀN HÌNH GƯƠNG CỰC SANG TRỌNG - 2 CỔNG SẠC NHANH + ĐÈN LED SIÊU SÁNG TẦM NHÌN TỐT - SẠC DỰ PHÒNG AN TOÀN CHO MỌI DÒNG ĐIỆN THOẠI",
+        "gia":100000,
+        "giam":49
+    },
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/4432dae01313320239c09b92f39baae2.jpg_400x400q80.jpg",
+        "thongtin":"DECROOM Hộp Đựng Quần Áo,giỏ lưu trữ khung thép vải lanh cotton Khung thép đậm Mật mã dày, không có mùi",
+        "gia":109000,
+        "giam":20
+    },
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/4d5058a78d9bea801be3035243b781bc.jpg_400x400q80.jpg",
+        "thongtin":"Máy rửa xe cầm tay dùng pin 199V, Máy rửa xe 2 pin 199V, Áp lực mạnh mẽ, pin 32000mAh sạc bền bỉ, 3 chế độ phun rửa, Chuyên sử dụng vệ sinh ô tô, xe máy, vật Dụng Gia Đình, Máy rửa xe mimi, máy rửa xe ap lực cao",
+        "gia":190000,
+        "giam":51
+    },
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/1469ed3d3e1f28729ae1b2d07f0c5fac.jpg_400x400q80.jpg",
+        "thongtin":"Lạp xưởng Tươi Loại Hảo Hạng 90% Nạc Đùi Heo. Đặc Sản Minh Triết( 1kg, 500gr)",
+        "gia":140000,
+        "giam":40
+    },
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/7b46f3dfff389102d8731bc469dd34a0.jpg_400x400q80.jpg",
+        "thongtin":"PIN DỰ PHÒNG 20000MAH CAO CẤP - SẠC DỰ PHÒNG MINI 4IN1 THIẾT KẾ HIỆN ĐẠI SANG TRỌNG - PIN SẠC DỰ PHÒNG TÍCH HỢP 2 ĐÈN LED , HIỂN THỊ PHẦN TRĂM PIN, BH 1 ĐỔI 1 TROG VÒNG 7 NGÀY",
+        "gia":220000,
+        "giam":30
+    },
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/32cc1a6a9a4df4af22d06c5b9e0df7a1.jpg_400x400q80.jpg_.webp",
+        "thongtin":"Socola hộp 408g ngon lắm nhé. Khuyến mãi giá cực sốc",
+        "gia":100000,
+        "giam":55
+    } 
+]
---
+++ item/item2.js
@@ -0,0 +1,38 @@
+[
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/cec3573ae164b42fc902773a7d69f3d5.png_400x400q80.png",
+        "tenshop":" Thảo Nguyên Shop ",
+        "logo":"https://lzd-img-global.slatic.net/g/shop/85843141db5651c3e46c8c63072e9bb9.jpeg_2200x2200q80.jpg"
+        
+    },
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/90d9663c089fc6a96ed80f2bb426e329.png_400x400q80.png",
+        "tenshop":" Thien Huong Foods ",
+        "logo":"https://lzd-img-global.slatic.net/g/shop/60a3b4d21ed9152a0f910f9ad03750b9.jpeg_2200x2200q80.jpg"
+        
+    },
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/c91a1acc26625e04731be57f53e91ab1.jpg_400x400q80.jpg_.webp",
+        "tenshop":" Anello ",
+        "logo":"https://lzd-img-global.slatic.net/g/shop/ff02bdbac43136895f66f7b5a5dc318a.jpeg_2200x2200q80.jpg"
+       
+    },
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/37659afd8b724a70462ae8b707463045.jpg_400x400q80.jpg",
+        "tenshop":" Dosreal ",
+        "logo":"https://lzd-img-global.slatic.net/g/shop/c1c137616ef974aa66b313ade0be89ce.jpeg_2200x2200q80.jpg"
+        
+    },
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/8f3f64c8b5c87a6268d4295615527cc0.jpg_400x400q80.jpg",
+        "tenshop":" Kymdan ",
+        "logo":"https://lzd-img-global.slatic.net/g/shop/504fa8fc401ef8af8990f56e7c13fa3f.png_2200x2200q80.png"
+        
+    },
+    {
+        "anh":"https://lzd-img-global.slatic.net/g/p/2910a069442227e89712101e254d2896.jpg_400x400q80.jpg",
+        "tenshop":" DK Harvest ",
+        "logo":"https://lzd-img-global.slatic.net/g/shop/5e04cac41e462681117f4c12b2395580.png_2200x2200q80.png"
+       
+    } 
+]
